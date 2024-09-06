9fb89d587ca4f5445eb67b51f48c1097e414ec27
MARTIN ARAN <47572118@ort>
2024-09-06 10:38:47 -0300

    tp terminado

--- App.js
+++ App.js
@@ -1,20 +1,227 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, TextInput, Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+
+function ScreenA1({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screenPink}>
+      <Text style={styles.text}>Pantalla Home 1</Text>
+      <Button title="Ir a la siguiente pantalla" onPress={() => navigation.navigate('ScreenA2')} />
     </View>
   );
 }
 
+function ScreenA2() {
+  return (
+    <View style={styles.screenPink}>
+      <Text style={styles.text}>Pantalla Home 2</Text>
+    </View>
+  );
+}
+
+
+function ScreenB1({ navigation }) {
+  const [nombre, setNombre] = React.useState('');
+  const [telefono, setTelefono] = React.useState('');
+
+  return (
+    <View style={styles.screenWhite}>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Teléfono"
+        value={telefono}
+        onChangeText={setTelefono}
+      />
+      <Button
+        title="Confirmar"
+        onPress={() => navigation.navigate('ScreenB2', { nombre, telefono })}
+      />
+    </View>
+  );
+}
+
+function ScreenB2({ route, navigation }) {
+  const { nombre, telefono } = route.params;
+  return (
+    <View style={styles.screenWhite}>
+      <Text style={styles.text}>¡Hola {nombre}!</Text>
+      <Text style={styles.text}>Tu numero de teléfono es {telefono}</Text>
+      <Button title="Volver" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+
+function ScreenC1({ navigation }) {
+  return (
+    <View style={styles.screenYellow}>
+      <Text style={styles.text}>Pantalla Perfil 1</Text>
+      <Button title="Ir a la siguiente pagina" onPress={() => navigation.navigate('ScreenC2')} />
+    </View>
+  );
+}
+
+function ScreenC2() {
+  return (
+    <View style={styles.screenYellow}>
+      <Text style={styles.text}>Pantalla Perfil 2</Text>
+    </View>
+  );
+}
+
+
+function ScreenD1({ navigation }) {
+  return (
+    <View style={styles.screenGreen}>
+      <Text style={styles.text}>Pantalla Configuración 1</Text>
+      <Button title="Ir a la siguiente pagina" onPress={() => navigation.navigate('ScreenD2')} />
+    </View>
+  );
+}
+
+function ScreenD2() {
+  return (
+    <View style={styles.screenGreen}>
+      <Text style={styles.text}>Pantalla Configuración 2</Text>
+    </View>
+  );
+}
+
+
+const StackA = createNativeStackNavigator();
+const StackB = createNativeStackNavigator();
+const StackC = createNativeStackNavigator();
+const StackD = createNativeStackNavigator();
+
+function StackANavigator() {
+  return (
+    <StackA.Navigator>
+      <StackA.Screen name="ScreenA1" component={ScreenA1} />
+      <StackA.Screen name="ScreenA2" component={ScreenA2} />
+    </StackA.Navigator>
+  );
+}
+
+function StackBNavigator() {
+  return (
+    <StackB.Navigator>
+      <StackB.Screen name="ScreenB1" component={ScreenB1} />
+      <StackB.Screen name="ScreenB2" component={ScreenB2} />
+    </StackB.Navigator>
+  );
+}
+
+function StackCNavigator() {
+  return (
+    <StackC.Navigator>
+      <StackC.Screen name="ScreenC1" component={ScreenC1} />
+      <StackC.Screen name="ScreenC2" component={ScreenC2} />
+    </StackC.Navigator>
+  );
+}
+
+function StackDNavigator() {
+  return (
+    <StackD.Navigator>
+      <StackD.Screen name="ScreenD1" component={ScreenD1} />
+      <StackD.Screen name="ScreenD2" component={ScreenD2} />
+    </StackD.Navigator>
+  );
+}
+
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={StackANavigator}
+        options={{
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Buscador"
+        component={StackBNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <Ionicons name="search" size={24} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={StackCNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <Ionicons name="person" size={24} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Configuración"
+        component={StackDNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <Ionicons name="settings" size={24} color={color} />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: '80%',
+  },
+  screenPink: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fca5a5',
+  },
+  screenWhite: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  screenYellow: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fcd34d',
+  },
+  screenGreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#86efac',
   },
 });
